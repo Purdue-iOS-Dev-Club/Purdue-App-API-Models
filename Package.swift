fdc0041210be78025eee6eb77db0979c8dbf0573
Anderson <andersondavid2000@gmail.com>
2021-07-18 11:48:43 -0700

    Added iOS 14 as a platform

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "Purdue-App-API-Models",
-    platforms: [.macOS(.v10_13)],
+    platforms: [.macOS(.v10_13), .iOS(.v14)],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
