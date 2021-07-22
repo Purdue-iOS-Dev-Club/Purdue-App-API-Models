22bb13031a909bff40a871e4997eef4832aca4a2
Anderson <andersondavid2000@gmail.com>
2021-07-21 22:16:29 -0700

    fixed null parsing problem with retail

--- Sources/Purdue-App-API-Models/Models/Dining/RetailLocation.swift
+++ Sources/Purdue-App-API-Models/Models/Dining/RetailLocation.swift
@@ -54,34 +54,21 @@ public struct RetailLocation: Codable {
     
     public init(from decoder: Decoder) throws {
         guard let container = try? decoder.container(keyedBy: CodingKeys.self) else {
-            print("Bad container")
             throw DiningError.unknownError
         }
         
-        print("Trying to get name")
-        let name = try container.decode(String.self, forKey: .Name)
-        print("Trying to get address")
-        let address = try container.decode(Address.self, forKey: .LocationAddress)
-        print("Trying to get phone")
-        let phoneNum = try container.decode(String.self, forKey: .PhoneNumber)
-        print("Trying to get lat")
-        let latitude = try container.decode(Float.self, forKey: .Latitude)
-        print("Trying to get long")
-        let longitude = try container.decode(Float.self, forKey: .Longitude)
-        print("Trying to get locType")
-        let locationType = try container.decode(LocationType.self, forKey: .RetailLocationType)
-        print("Trying to get logo")
-        let logoUrl = try container.decode(URL.self, forKey: .LogoUrl)
-        print("Trying to get menu")
-        let menuUrl = try container.decode(URL.self, forKey: .MenuUrl)
-        print("Trying to get desc")
-        let description = try container.decode(String.self, forKey: .Description)
-        print("Trying to get url")
-        let url = try container.decode(URL.self, forKey: .Url)
-        print("Trying to get banner")
-        let bannerUrl = try container.decode(URL.self, forKey: .BannerUrl)
+        let name = try? container.decode(String.self, forKey: .Name)
+        let address = try? container.decode(Address.self, forKey: .LocationAddress)
+        let phoneNum = try? container.decode(String.self, forKey: .PhoneNumber)
+        let latitude = try? container.decode(Float.self, forKey: .Latitude)
+        let longitude = try? container.decode(Float.self, forKey: .Longitude)
+        let locationType = try? container.decode(LocationType.self, forKey: .RetailLocationType)
+        let logoUrl = try? container.decode(URL.self, forKey: .LogoUrl)
+        let menuUrl = try? container.decode(URL.self, forKey: .MenuUrl)
+        let description = try? container.decode(String.self, forKey: .Description)
+        let url = try? container.decode(URL.self, forKey: .Url)
+        let bannerUrl = try? container.decode(URL.self, forKey: .BannerUrl)
         
         self.init(name: name, address: address, phoneNum: phoneNum, latitude: latitude, longitude: longitude, locationType: locationType, logoUrl: logoUrl, menuUrl: menuUrl, description: description, url: url, bannerUrl: bannerUrl)
-        print(self)
     }
 }
