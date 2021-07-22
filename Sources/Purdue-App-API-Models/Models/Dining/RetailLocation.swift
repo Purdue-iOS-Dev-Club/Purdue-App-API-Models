b46468aee375159d0f76c65adc5b1ab1f72ec4dc
Anderson <andersondavid2000@gmail.com>
2021-07-21 21:44:24 -0700

    more debugging

--- Sources/Purdue-App-API-Models/Models/Dining/RetailLocation.swift
+++ Sources/Purdue-App-API-Models/Models/Dining/RetailLocation.swift
@@ -13,11 +13,11 @@ public struct RetailLocationResponse: Codable {
 
 public struct RetailLocation: Codable {
     public let Name: String?
-    public let Address: Address?
+    public let LocationAddress: Address?
     public let PhoneNumber: String?
     public let Latitude: Float?
     public let Longitude: Float?
-    public let LocationType: LocationType?
+    public let RetailLocationType: LocationType?
     public let LogoUrl: URL?
     public let MenuUrl: URL?
     public let Description: String?
@@ -26,11 +26,11 @@ public struct RetailLocation: Codable {
     
     public enum CodingKeys: String, CodingKey {
         case Name
-        case Address
+        case LocationAddress = "Address"
         case PhoneNumber
         case Latitude
         case Longitude
-        case LocationType = "Type"
+        case RetailLocationType = "Type"
         case LogoUrl
         case MenuUrl
         case Description
@@ -40,11 +40,11 @@ public struct RetailLocation: Codable {
     
     public init(name: String?, address: Address?, phoneNum: String?, latitude: Float?, longitude: Float?, locationType: LocationType?, logoUrl: URL?, menuUrl: URL?, description: String?, url: URL?, bannerUrl: URL?) {
         self.Name = name
-        self.Address = address
+        self.LocationAddress = address
         self.PhoneNumber = phoneNum
         self.Latitude = latitude
         self.Longitude = longitude
-        self.LocationType = locationType
+        self.RetailLocationType = locationType
         self.LogoUrl = logoUrl
         self.MenuUrl = menuUrl
         self.Description = description
@@ -59,11 +59,11 @@ public struct RetailLocation: Codable {
         }
         
         let name = try? container.decode(String.self, forKey: .Name)
-        // let address = try? container.decode(Address.self, forKey: .Address)
+        let address = try? container.decode(Address.self, forKey: .LocationAddress)
         let phoneNum = try? container.decode(String.self, forKey: .PhoneNumber)
         let latitude = try? container.decode(Float.self, forKey: .Latitude)
         let longitude = try? container.decode(Float.self, forKey: .Longitude)
-        // let locationType = try? container.decode(LocationType, forKey: .LocationType)
+        let locationType = try? container.decode(LocationType.self, forKey: .RetailLocationType)
         let logoUrl = try? container.decode(URL.self, forKey: .LogoUrl)
         let menuUrl = try? container.decode(URL.self, forKey: .MenuUrl)
         let description = try? container.decode(String.self, forKey: .Description)
@@ -71,6 +71,6 @@ public struct RetailLocation: Codable {
         let bannerUrl = try? container.decode(URL.self, forKey: .BannerUrl)
         
         self.init(name: name, address: nil, phoneNum: phoneNum, latitude: latitude, longitude: longitude, locationType: nil, logoUrl: logoUrl, menuUrl: menuUrl, description: description, url: url, bannerUrl: bannerUrl)
-        
+        print(self)
     }
 }
