0cbf287850ab9ac94304e76d5f3b1ef70934d7e5
Anderson David <andersondavid2000@gmail.com>
2021-08-17 13:03:57 -0400

    added directory constructor

--- Sources/Purdue-App-API-Models/Models/Directory/Directory.swift
+++ Sources/Purdue-App-API-Models/Models/Directory/Directory.swift
@@ -14,4 +14,13 @@ public struct DirectoryResult: Codable {
     public let school: String
     public let qualifiedName: String
     public let email: String
+    
+    public init(name: String = "", alias: String = "", campus: String = "", school: String = "", qualifiedName: String = "", email: String = "") {
+        self.name = name
+        self.alias = alias
+        self.campus = campus
+        self.school = school
+        self.qualifiedName = qualifiedName
+        self.email = email
+    }
 }
