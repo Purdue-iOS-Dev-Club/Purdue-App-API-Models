86af722020b6ab15efb978f16318a0189e72aae8
Anderson David <andersondavid2000@gmail.com>
2021-08-17 12:01:08 -0400

    added directory model

---
+++ Sources/Purdue-App-API-Models/Models/Directory/Directory.swift
@@ -0,0 +1,17 @@
+//
+//  File.swift
+//  
+//
+//  Created by Anderson David on 8/17/21.
+//
+
+import Foundation
+
+struct Result: Codable {
+    let name: String
+    let alias: String
+    let campus: String
+    let school: String
+    let qualifiedName: String
+    let email: String
+}
