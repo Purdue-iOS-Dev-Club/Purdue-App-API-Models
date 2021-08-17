8b84a2e07760bea01ece460b3a76964487af0b48
Anderson David <andersondavid2000@gmail.com>
2021-08-17 12:12:50 -0400

    added directory url

---
+++ Sources/Purdue-App-API-Models/Helpers/DirectoryHelpers.swift
@@ -0,0 +1,12 @@
+//
+//  File.swift
+//  
+//
+//  Created by Anderson David on 8/17/21.
+//
+
+import Foundation
+
+public class DirectoryHelpers {
+    public static let baseURL: String = "https://www.purdue.edu/directory/?searchString="
+}
