c14ecf1cc8562909313f80a89e3637772ff1a9dd
andersondav <andersondavid2000@gmail.com>
2021-08-18 13:00:59 -0400

    added url for laundry

---
+++ Sources/Purdue-App-API-Models/Helpers/LaundryHelpers.swift
@@ -0,0 +1,12 @@
+//
+//  File.swift
+//  
+//
+//  Created by Anderson David on 8/18/21.
+//
+
+import Foundation
+
+public class LaundryHelpers {
+    static let baseUrl = "https://wpvappsuds01.boilerad.purdue.edu/washalertweb/washalertweb.aspx?_ga=2.223485342.467914990.1629134131-2041941301.1627575032"
+}
