7d3444ec678b1dce6dcb28ffccaf340e227f4025
andersondav <andersondavid2000@gmail.com>
2021-08-18 13:02:17 -0400

    made url public

--- Sources/Purdue-App-API-Models/Helpers/LaundryHelpers.swift
+++ Sources/Purdue-App-API-Models/Helpers/LaundryHelpers.swift
@@ -8,5 +8,5 @@
 import Foundation
 
 public class LaundryHelpers {
-    static let baseUrl = "https://wpvappsuds01.boilerad.purdue.edu/washalertweb/washalertweb.aspx?_ga=2.223485342.467914990.1629134131-2041941301.1627575032"
+    public static let baseUrl = "https://wpvappsuds01.boilerad.purdue.edu/washalertweb/washalertweb.aspx?_ga=2.223485342.467914990.1629134131-2041941301.1627575032"
 }
